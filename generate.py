255bfc570918869584910dbd3b0d8fc62afe05f1
James Shaw <js102@zepler.net>
2017-10-18 12:03:09 +0100

    add generate.py for mc-report-2017-12

---
+++ generate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from ninjaopenfoam import Build, PDFLaTeX, Shortcuts
+import os
+
+class MonitoringCommittee:
+    def __init__(self):
+        self.build = Build()
+
+    def write(self):
+        build = self.build
+
+        report = PDFLaTeX(
+                'report',
+                output=os.path.join('mc-report-2017-12/mc-report-2017-12'),
+                document=os.path.join('src/mc-report-2017-12/mc-report-2017-12'))
+
+        shortcuts = Shortcuts([report.output])
+
+        build.add(report)
+        build.add(shortcuts)
+
+        build.write()
+
+
+if __name__ == '__main__':
+    MonitoringCommittee().write()
+
